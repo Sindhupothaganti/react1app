8b959fa0cbb9b6ffe4d8394283736c14afb7545c
Sindhupothaganti <sindhupothaganti17.com>
2021-11-09 16:29:33 +0530

    variable message

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -1,31 +1,22 @@
 import React, { Component } from 'react'
-
 export class UserGreeting extends Component {
     constructor(props) {
         super(props)
-    
+
         this.state = {
-             isLoggedIn : false
+             isLoggedIn :true
         }
     }
-    
+
     render() {
-
-       return this.state.isLoggedIn && <div>welcome ansari</div>
-       /*  return(
-            this.state.isLoggedIn ?
-            <div>welcome abdul</div> :
-            <div>welcome guest</div>
-        ) */
-
-       /*  let message 
+        let message 
         if(this.state.isLoggedIn){
-            message = <div>welcome abdul</div>
+            message = <div>welcome Sindhu pothaganti</div>
         }
         else{
             message = <div>welcome guest</div>
         }
-        return <div>{message}</div> */
+        return <div>{message}</div>
        /*  if(this.state.isLoggedIn){
             return <div>welcome abdul</div>
         }
@@ -40,5 +31,4 @@ export class UserGreeting extends Component {
         ) */
     }
 }
-
 export default UserGreeting
