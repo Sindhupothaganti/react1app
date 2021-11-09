02a68079875db05fd612e8146e20195b98648f7b
Sindhupothaganti <sindhupothaganti17.com>
2021-11-09 16:04:08 +0530

    conditional rendering on if/else

--- src/App.js
+++ src/App.js
@@ -1,39 +1,3 @@
-/* import logo from './logo.svg';
-import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
- 
-
-function App() {
-  return (
-    <div className="App">
-<EventBind/>
-    <Counter/>
-
-    <Message/>
-
-    <Greet name = "ram"  college = "IIT">
-      <p> this is children property</p>
-    </Greet>
-
-    <Greet name = "sham" college = "Oxford">
-      <button>click me</button>
-    </Greet>
-    <Greet />
-
-    <Welcome name ="sindhu" alias = "sindh"/>
-
-    </div>
-  );
-}
-
-export default App; 
- */
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
@@ -44,11 +8,14 @@ import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
 import EventBind from './components/EventBind';
 import ParentComponent from './components/ParentComponent';
+import UserGreeting from './components/UserGreeting';
  
 
 function App() {
   return (
     <div className="App">
+    <UserGreeting/>
+
     <ParentComponent/>
     <EventBind/>
     
