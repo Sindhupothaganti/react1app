7778580d408d7368585a00044bde177b180b6042
Sindhupothaganti <sindhupothaganti17.com>
2021-11-10 12:04:06 +0530

    inline styling

--- src/components/Stylesheet.js
+++ src/components/Stylesheet.js
@@ -2,11 +2,12 @@ import React from 'react'
 import './myStyles.css'
 
 function Stylesheet(props) {
-    
+    //literal-$, template --{} and attribute-name
+
     let className = props.enabled ? 'primary' : ''
     return (
         <div>
-            <h1 className = {className}> welcome to style sheets</h1>
+            <h1 className = {`${className} font-xl`}> welcome to style sheets</h1>
         </div>
     )
 }
